932d82dd026651c679754647cf59a9ba19f4d066
nik <129772745+nikcode9@users.noreply.github.com>
2024-06-20 01:38:03 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Directly fetch weather for New Delhi, India
+    const lat = 28.6139; // Latitude for New Delhi
+    const lon = 77.2090; // Longitude for New Delhi
+    getWeather(lat, lon);
+  });
+  
+  function getWeather(lat, lon) {
+    const apiKey = '77565c3622a728305f9fa447e584db9b';  // Replace with your weather API key
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById('location').innerText = `${data.name}, ${data.sys.country}`;
+        document.getElementById('temperature').innerText = `${data.main.temp} °C`;
+        document.getElementById('conditions').innerText = data.weather[0].description;
+      })
+      ;
+  }
+  
